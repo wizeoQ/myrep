d4e1146009375cf01dfbe364b9bb2a8f64b741f3
wizeoQ <quartetqoc@gmail.com>
2026-04-23 10:06:30 +0300

    Создан первый файл проекта

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 08:38:28 2026
+
+@author: wizeo
+"""
+
+my_name_is = "Who?"
+print(my_name_is)
+my_name_is = "What?"
+print(my_name_is)
+my_name_is = "WHY"
+print (my_name_is)
+print ("Slim shady")
